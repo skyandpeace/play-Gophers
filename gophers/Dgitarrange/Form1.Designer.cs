396767a2b6bb544485e5ac1d452400e9d5d62dab
meow <johnkikijoy@gmail.com>
2019-06-09 13:00:37 +0800

    updata

---
+++ gophers/Dgitarrange/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Dgitarrange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrout = new System.Windows.Forms.Timer(this.components);
+            this.btnstart = new System.Windows.Forms.Button();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.easy = new System.Windows.Forms.RadioButton();
+            this.hard = new System.Windows.Forms.RadioButton();
+            this.evil = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rebtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrout
+            // 
+            this.tmrout.Tick += new System.EventHandler(this.tmrout_Tick);
+            // 
+            // btnstart
+            // 
+            this.btnstart.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnstart.Location = new System.Drawing.Point(730, 338);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(151, 44);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "啟動遊戲";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Font = new System.Drawing.Font("新細明體", 24F);
+            this.lblscore.Location = new System.Drawing.Point(730, 15);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(102, 32);
+            this.lblscore.TabIndex = 1;
+            this.lblscore.Text = "分數:0";
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Font = new System.Drawing.Font("新細明體", 24F);
+            this.lbltime.Location = new System.Drawing.Point(730, 61);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(117, 32);
+            this.lbltime.TabIndex = 2;
+            this.lbltime.Text = "時間:60";
+            // 
+            // easy
+            // 
+            this.easy.AutoSize = true;
+            this.easy.Location = new System.Drawing.Point(34, 23);
+            this.easy.Name = "easy";
+            this.easy.Size = new System.Drawing.Size(47, 16);
+            this.easy.TabIndex = 3;
+            this.easy.Text = "簡單";
+            this.easy.UseVisualStyleBackColor = true;
+            this.easy.CheckedChanged += new System.EventHandler(this.easy_CheckedChanged);
+            // 
+            // hard
+            // 
+            this.hard.AutoSize = true;
+            this.hard.Location = new System.Drawing.Point(34, 46);
+            this.hard.Name = "hard";
+            this.hard.Size = new System.Drawing.Size(47, 16);
+            this.hard.TabIndex = 4;
+            this.hard.Text = "困難";
+            this.hard.UseVisualStyleBackColor = true;
+            // 
+            // evil
+            // 
+            this.evil.AutoSize = true;
+            this.evil.Location = new System.Drawing.Point(34, 68);
+            this.evil.Name = "evil";
+            this.evil.Size = new System.Drawing.Size(47, 16);
+            this.evil.TabIndex = 5;
+            this.evil.Text = "魔鬼";
+            this.evil.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.evil);
+            this.groupBox1.Controls.Add(this.easy);
+            this.groupBox1.Controls.Add(this.hard);
+            this.groupBox1.Location = new System.Drawing.Point(736, 131);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(111, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "難度";
+            // 
+            // rebtn
+            // 
+            this.rebtn.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rebtn.Location = new System.Drawing.Point(730, 288);
+            this.rebtn.Name = "rebtn";
+            this.rebtn.Size = new System.Drawing.Size(151, 44);
+            this.rebtn.TabIndex = 7;
+            this.rebtn.Text = "重新開始";
+            this.rebtn.UseVisualStyleBackColor = true;
+            this.rebtn.Click += new System.EventHandler(this.rebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 455);
+            this.Controls.Add(this.rebtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.btnstart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Timer tmrout;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.RadioButton easy;
+        private System.Windows.Forms.RadioButton hard;
+        private System.Windows.Forms.RadioButton evil;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button rebtn;
+    }
+}
+
---
+++ gophers/Dgitarrange/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dgitarrange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //inGophers躲進去的時間 outGophers出來時間 scoreGophers地鼠的分數
+        int go_out, inGophers, outGophers,scoreGophers;
+        //time 時間 end 結束時間
+        int time = 0, end = 60000;
+        int score = 0;
+        Random rd = new Random();
+        //生成類別變數
+        Button btn1;
+        Button[,] btndigit = new Button[3, 3];
+        RadioButton rdbtn1;
+        Panel PnlPlatter = new Panel();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //自行宣告按鈕，非自己拉的
+            this.WindowState = FormWindowState.Maximized;//螢幕表單最大化
+            this.Controls.Add(PnlPlatter);//加入到form表單
+            PnlPlatter.Top = 15;
+            PnlPlatter.Left = 15;
+            PnlPlatter.Width = 630;
+            PnlPlatter.Height = 630;
+            PnlPlatter.BackColor = Color.Aqua;
+            PnlPlatter.Enabled = false;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    btndigit[i, j] = new Button();
+                    //加入panel裡面
+                    PnlPlatter.Controls.Add(btndigit[i, j]);
+                    //取名字
+                    btndigit[i, j].Name = "btn" + i.ToString() + j.ToString();
+                    btndigit[i, j].Text = "";
+                    btndigit[i, j].Width = 200;
+                    btndigit[i, j].Height = 200;
+                    btndigit[i, j].Font = new Font("Times New Roman", 24, FontStyle.Bold);
+                    btndigit[i, j].Top = 15 + i * 200;
+                    btndigit[i, j].Left = 15 + j * 200;
+                    btndigit[i, j].BackColor = Color.White;
+                    //當很多物件做相同的事情，使用訂閱，減化程式碼
+                    btndigit[i, j].Click += new EventHandler(Btn00_Click);
+                }
+            hard.CheckedChanged += new EventHandler(easy_CheckedChanged);
+            evil.CheckedChanged += new EventHandler(easy_CheckedChanged);
+        }
+        //因是自行產生，需自行寫入，通常是複製別人的header
+        private void Btn00_Click(object sender, EventArgs e)
+        {
+            btn1 = (Button)sender;
+            if (btn1.BackgroundImage != null)
+            {
+                btn1.BackgroundImageLayout = ImageLayout.Stretch;
+                btn1.BackgroundImage = new Bitmap("out.png");
+                score += scoreGophers;
+                lblscore.Text = "分數:" + score.ToString();
+            }
+        }
+
+        private void rebtn_Click(object sender, EventArgs e)
+        {
+            PnlPlatter.Enabled = false;
+            tmrout.Enabled = false;
+            btnstart.Enabled = true;
+            groupBox1.Enabled = true;
+            score = 0;
+            end = 60000;
+            time = 0;
+            lblscore.Text = "分數:0";
+            lbltime.Text = "時間:60";
+            lbltime.ForeColor = Color.Black;
+            for (int i = 0; i < 9; i++)
+                PnlPlatter.Controls[i].BackgroundImage = null;
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            PnlPlatter.Enabled = true;
+            tmrout.Enabled = true;
+            btnstart.Enabled = false;
+            groupBox1.Enabled = false;
+        }
+
+        private void easy_CheckedChanged(object sender, EventArgs e)
+        {
+            //使用類別變數，指定目前的類別變數是哪個物件(指定的物件有可能不同)
+            rdbtn1 = (RadioButton)sender;
+            if (rdbtn1.Text == "簡單")
+            {
+                inGophers = 2000;
+                outGophers = 1000;
+                scoreGophers = 100;
+            }
+            else if (rdbtn1.Text == "困難")
+            {
+                inGophers = 1000;
+                outGophers = 500;
+                scoreGophers = 100;
+            }
+            else 
+            {
+                inGophers = 400;
+                outGophers = 200;
+                scoreGophers = 500;
+            }
+        }
+
+        private void tmrout_Tick(object sender, EventArgs e)
+        {
+            lbltime.Text = "時間:" + ((end -= tmrout.Interval) / 1000).ToString();
+            time += tmrout.Interval;
+            if (end / 1000 == 15)
+                lbltime.ForeColor = Color.Red;
+            if (end / 1000 == 0)
+            {
+                tmrout.Enabled = false;
+                btnstart.Enabled = true;
+                MessageBox.Show("遊戲結束 分數:" + score.ToString(), "打地鼠");
+                end = 60000;
+                score = 0;
+                time = 0;
+                lblscore.Text = "分數:0";
+                lbltime.Text = "時間:60";
+                lbltime.ForeColor = Color.Black;
+                for (int i = 0; i < 9; i++)
+                    PnlPlatter.Controls[i].BackgroundImage = null;
+            }
+            if (time == inGophers)
+            {
+                time = 0;
+                PnlPlatter.Controls[go_out].BackgroundImage = null;
+            }
+            else if (time == outGophers && PnlPlatter.Controls[go_out].BackgroundImage == null)
+            {
+                go_out = rd.Next(9);
+                PnlPlatter.Controls[go_out].BackgroundImageLayout = ImageLayout.Stretch;
+                PnlPlatter.Controls[go_out].BackgroundImage = new Bitmap("in.gif");
+            }
+        }
+    }
+}
